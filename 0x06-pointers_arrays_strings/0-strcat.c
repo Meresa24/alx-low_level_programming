fd897263b75903045d4d3bed5d814dbcf83ac8f4
meresa24 <meresagidey2019@gmail.com>
2022-09-22 19:26:39 -0700

    commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,42 +1,26 @@
 #include "main.h"
 
 /**
- * print_number - function to print int using _putchar
+ * _strcat - function to concatenate two strings and return a new string
  *
- * @num: int to print
+ * @dest: string 1
+ * @src: string 2
+ * Return: char pointer
  */
-void print_number(int num)
+char *_strcat(char *dest, char *src)
 {
-	unsigned int d = 10, n;
+	int i = 0, j = 0;
+	char *p;
 
-	if (num < 0)
+	while (*(dest + i) != '\0')
+		i++;
+	while (*(src + j) != '\0')
 	{
-		_putchar('-');
-		num *= -1;
+		*(dest + i) = *(src + j);
+		i++;
+		j++;
 	}
-	n = num;
-	if (n < d)
-	{
-		_putchar('0' + n);
-	}
-	else
-	{
-		while (n >= d)
-		{
-			d *= 10;
-			if (d >= 1000000000)
-			if (d == 1000000000)
-				break;
-		}
-		if (!(d >= 1000000000) || n > 100000000)
-		if (!(d == 1000000000) || n == 123456789)
-			d /= 10;
-		_putchar('0' + n / d);
-		while (d != 10)
-		{
-			d /= 10;
-			_putchar('0' + (n / d) % 10);
-		}
-		_putchar('0' + n % 10);
-	}
+	*(dest + i) = '\0';
+	p = dest;
+	return (p);
 }
